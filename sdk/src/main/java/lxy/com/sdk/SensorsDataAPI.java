a65aeea32452dc3306eafbd5a6cfc837e6c9bf87
lxy8250 <1445925233@qq.com>
2019-06-10 00:14:25 +0800

    优化

--- sdk/src/main/java/lxy/com/sdk/SensorsDataAPI.java
+++ sdk/src/main/java/lxy/com/sdk/SensorsDataAPI.java
@@ -1,8 +1,30 @@
 package lxy.com.sdk;
 
+import android.app.Application;
+
 /**
  * Creator : lxy
  * date: 2019/6/10
  */
 public class SensorsDataAPI {
+    private final String TAG = this.getClass().getSimpleName();
+    private static SensorsDataAPI instance;
+    private static final Object mLock = new Object();
+
+    private SensorsDataAPI(Application application){
+
+    }
+
+    public static SensorsDataAPI init(Application application){
+        synchronized (mLock){
+            if (instance == null){
+                instance = new SensorsDataAPI(application);
+            }
+            return instance;
+        }
+    }
+
+    public static SensorsDataAPI getInstance(){
+        return instance;
+    }
 }
